95b5a9c395147a034ea6a13f6bd6563bad6be5a7
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-02-18 14:20:52 -0500

    complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python3
 
+# outputs numbers starting at 10 and counting down to 1
 def happy_new_year():
-    # code goes here!
-    pass
+    i=10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
+    # pass
 
+# use list comprehension
+# function takes in list of integers and returns list of squared elements
 def square_integers(int_list):
-    # code goes here!
-    pass
+    int_list = [(num * num) for num in int_list]
+    return int_list
+    # pass
 
+# prints numbers 1 to 100
+# for multiples of 3, print "Fizz"
+# for multiples of 5, print "Buzz"
+# for multiples of 3 and 5, print "FizzBuzz"
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if (i % 3 == 0) and (i % 5 == 0):
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+    # pass
